83ec2ae00d2b16b35a009d207640b0015e059640
quocngu <quocngu4616@gmail.com>
2020-11-19 21:01:06 +0700

    upfile

--- src/component/Search/InputSearch.js
+++ src/component/Search/InputSearch.js
@@ -1,45 +1,11 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { searchCourse } from '../../store/actions/action';
-class InputSearch extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            search: ''
-        }
-    }
-    onChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        }, () => {
-            this.props.onSearchCourse(this.state.search)
-        })
-    }
-    onSearch = () => {
-        // e.preventDefault();
-        this.props.onSearchCourse(this.state.search)
-    }
+
+export default class InputSearch extends Component {
     render() {
         return (
-            <form>
-                <div className="input-group mb-3">
-                    <input type="text" className="form-control" placeholder="Search for Course" name='search' value={this.state.search} onChange={this.onChange} />
-                    <div className="input-group-prepend" onClick={this.onSearch}>
-                        <span className="input-group-text"><i className="fa fa-search" /></span>
-                    </div>
-                </div>
-            </form>
+            <div>
+                
+            </div>
         )
     }
 }
-const mapDispatchToProps = dispatch => {
-    return {
-        onSearchCourse: (search) => {
-            return (
-                dispatch(searchCourse(search))
-            )
-
-        }
-    }
-}
-export default connect(null, mapDispatchToProps)(InputSearch)  
